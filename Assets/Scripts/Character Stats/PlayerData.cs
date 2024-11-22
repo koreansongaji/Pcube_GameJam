0b559f6188579c06cb16be1d57d2feddd78273cc
OZ <dwd0102@sju.ac.kr>
2024-11-23 04:57:12 +0900

    PlayerData에 현재 HP 추가

--- Assets/Scripts/Character Stats/PlayerData.cs
+++ Assets/Scripts/Character Stats/PlayerData.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Player Data", menuName = "Scriptable Object/Player Data", order = int.MaxValue)]
 public class PlayerData : ScriptableObject
 {
-    // 최대 체력, 초당 재생, 방어력, 피해량, 투사체 속도, 지속시간, 공격범위, 쿨타임, 투사체 수, 부활, 성장
     [SerializeField] public CharacterStat maxHp;
+    [SerializeField] public CharacterStat currentHp;
     [SerializeField] public CharacterStat regenHp;
     [SerializeField] public CharacterStat armor;
     [SerializeField] public CharacterStat damage;
