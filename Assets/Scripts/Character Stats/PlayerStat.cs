d4a87ce98c057a6ba040d9ca229569af861ea215
OZ <dwd0102@sju.ac.kr>
2024-11-23 04:28:17 +0900

    PlayerStat

--- Assets/Scripts/Character Stats/PlayerStat.cs
+++ Assets/Scripts/Character Stats/PlayerStat.cs
@@ -5,135 +5,15 @@ using UnityEngine;
 public class PlayerData : ScriptableObject
 {
     // 최대 체력, 초당 재생, 방어력, 피해량, 투사체 속도, 지속시간, 공격범위, 쿨타임, 투사체 수, 부활, 성장
-    [SerializeField]
-    private float _maxHp;
-    public float MaxHp
-    {
-        get => _maxHp;
-        set
-        {
-            _maxHp = value;
-            if (_maxHp < 0) _maxHp = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _hpRegen;
-    public float HpRegen
-    {
-        get => _hpRegen;
-        set
-        {
-            _hpRegen = value;
-            if (_hpRegen < 0) _hpRegen = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _defense;
-    public float Defense
-    {
-        get => _defense;
-        set
-        {
-            _defense = value;
-            if (_defense < 0) _defense = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _damage;
-    public float Damage
-    {
-        get => _damage;
-        set
-        {
-            _damage = value;
-            if (_damage < 0) _damage = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _projectileSpeed;
-    public float ProjectileSpeed
-    {
-        get => _projectileSpeed;
-        set
-        {
-            _projectileSpeed = value;
-            if (_projectileSpeed < 0) _projectileSpeed = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _duration;
-    public float Duration
-    {
-        get => _duration;
-        set
-        {
-            _duration = value;
-            if (_duration < 0) _duration = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _attackRange;
-    public float AttackRange
-    {
-        get => _attackRange;
-        set
-        {
-            _attackRange = value;
-            if (_attackRange < 0) _attackRange = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _coolTime;
-    public float CoolTime
-    {
-        get => _coolTime;
-        set
-        {
-            _coolTime = value;
-            if (_coolTime < 0) _coolTime = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _projectileCount;
-    public float ProjectileCount
-    {
-        get => _projectileCount;
-        set
-        {
-            _projectileCount = value;
-            if (_projectileCount < 0) _projectileCount = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _resurrection;
-    public float Resurrection
-    {
-        get => _resurrection;
-        set
-        {
-            _resurrection = value;
-            if (_resurrection < 0) _resurrection = 0;
-        }
-    }
-    
-    [SerializeField]
-    private float _growth;
-    public float Growth
-    {
-        get => _growth;
-        set
-        {
-            _growth = value;
-            if (_growth < 0) _growth = 0;
-        }
-    }
+    [SerializeField] public CharacterStat maxHp;
+    [SerializeField] public CharacterStat regenHp;
+    [SerializeField] public CharacterStat armor;
+    [SerializeField] public CharacterStat damage;
+    [SerializeField] public CharacterStat projectileSpeed;
+    [SerializeField] public CharacterStat duration;
+    [SerializeField] public CharacterStat attackRange;
+    [SerializeField] public CharacterStat coolTime;
+    [SerializeField] public CharacterStat projectileCount;
+    [SerializeField] public CharacterStat resurrection;
+    [SerializeField] public CharacterStat growth;
 }
