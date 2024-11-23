960367d0e8bbc6121e9231efe8df363cbf41921a
HeeJunSong <koreansongaji@gmail.com>
2024-11-23 15:24:11 +0900

    풀링시스템 임시로 완성

--- Assets/Scripts/Managers/PoolingManager.cs
+++ Assets/Scripts/Managers/PoolingManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,11 @@ using UnityEngine;
 public class PoolingManager : MonoBehaviour //이 스크립트는 싱글톤으로 안만들고 스폰 지점에 게임 오브젝트에 적용시킨다
 {
     public GameObject[] Prefabs; //몬스터 종류만 저장할 배열
-    public List<GameObject>[] Pools; //소환한 몬스터들을 저장할 배열
+    public List<GameObject>[] DeActive; //소환한 몬스터들을 저장할 배열
     public GameObject[] SpawnPoint;
 
-    private int CountLevel_1 = 0;
-    private int CountLevel_2 = 0;
-    private int CountLevel_3 = 0;
-    private int CountLevel_4 = 0;
-    private int IndexLevel_1 = 0;
-    private int IndexLevel_2 = 0;
-    private int IndexLevel_3 = 0;
-    private int IndexLevel_4 = 0;
-
+    private int[] CountMonster = new int[4];
+    private int[] TimeIndex = new int[4];
 
     public struct SpawnMonsterInfo
     {
@@ -28,7 +22,6 @@ public class PoolingManager : MonoBehaviour //이 스크립트는 싱글톤으로 안만들고 
             this.num = n;
         }
     }
-
 
     private SpawnMonsterInfo[] Level_1_Time = new SpawnMonsterInfo[]
     {
@@ -53,14 +46,15 @@ public class PoolingManager : MonoBehaviour //이 스크립트는 싱글톤으로 안만들고 
 
     private void SetUp()
     {
-        CountLevel_1 = 0;
-        CountLevel_2 = 0;
-        CountLevel_3 = 0;
-        CountLevel_4 = 0;
-        IndexLevel_1 = 0;
-        IndexLevel_2 = 0;
-        IndexLevel_3 = 0;
-        IndexLevel_4 = 0;
+        CountMonster[0] = 0;
+        CountMonster[1] = 0;
+        CountMonster[2] = 0;
+        CountMonster[3] = 0;
+
+        TimeIndex[0] = 0;
+        TimeIndex[1] = 0;
+        TimeIndex[2] = 0;
+        TimeIndex[3] = 0;
     }
     private void Awake()
     {
@@ -82,16 +76,13 @@ public class PoolingManager : MonoBehaviour //이 스크립트는 싱글톤으로 안만들고 
     /// </summary>
     private void Pooling()
     {
-        Pools = new List<GameObject>[Prefabs.Length];
+        DeActive = new List<GameObject>[Prefabs.Length];
 
         for (int index = 0; index < Prefabs.Length; index++)
         {
-            Pools[index] = new List<GameObject>();
+            DeActive[index] = new List<GameObject>();
         }
         InstantMonster(0, 100);
-        InstantMonster(1, 30);
-        InstantMonster(2, 30);
-        InstantMonster(3, 3);
     }
 
     /// <summary>
@@ -106,7 +97,7 @@ public class PoolingManager : MonoBehaviour //이 스크립트는 싱글톤으로 안만들고 
         for (int i = 0; i < count; i++)
         {
             select = Instantiate(Prefabs[index], this.transform);
-            Pools[index].Add(select);
+            DeActive[index].Add(select);
             select.SetActive(false);
         }
 
@@ -120,135 +111,54 @@ public class PoolingManager : MonoBehaviour //이 스크립트는 싱글톤으로 안만들고 
     {
         //if (GameManager.Instance != null && GameManager.Instance.time >= Level_1_Time[IndexLevel_1].time)
         {
-            for (int i = 0; i < Level_1_Time[IndexLevel_1].num; i++)
+            for (int i = 0; i < Level_1_Time[TimeIndex[0]].num; i++)
             {
-                SpawnLevel_1();
+                SetActiveMonster(0);
             }
-            IndexLevel_1++;
+            TimeIndex[0]++;
         }
         //if (GameManager.Instance != null && GameManager.Instance.time >= Level_2_Time[IndexLevel_2].time)
         {
-            for (int i = 0; i < Level_2_Time[IndexLevel_1].num; i++)
+            for (int i = 0; i < Level_2_Time[TimeIndex[1]].num; i++)
             {
-                SpawnLevel_2();
+                SetActiveMonster(1);
             }
-            IndexLevel_2++;
+            TimeIndex[1]++;
         }
         //if (GameManager.Instance != null && GameManager.Instance.time >= Level_3_Time[IndexLevel_3].time)
         {
-            for (int i = 0; i < Level_3_Time[IndexLevel_1].num; i++)
+            for (int i = 0; i < Level_3_Time[TimeIndex[2]].num; i++)
             {
-                SpawnLevel_3();
+                SetActiveMonster(2);
             }
-            IndexLevel_3++;
+            TimeIndex[2]++;
         }
         //if (GameManager.Instance != null && GameManager.Instance.time >= Level_4_Time[IndexLevel_4].time)
         {
-            for (int i = 0; i < Level_4_Time[IndexLevel_1].num; i++)
+            for (int i = 0; i < Level_4_Time[TimeIndex[3]].num; i++)
             {
-                SpawnLevel_4();
+                SetActiveMonster(3);
             }
-            IndexLevel_4++;
+            TimeIndex[3]++;
         }
     }
-
     /// <summary>
-    /// 레벨 1 몬스터 활성화
+    /// 풀링한 몬스터 활성화 시키는 함수
     /// </summary>
+    /// <param name="monsterKind">어떤 종류의 몬스터를 활성화 시킬지 정함</param>
     /// <returns></returns>
-    private GameObject SpawnLevel_1()
+    private GameObject SetActiveMonster(int monsterKind)
     {
         GameObject spawn = null;
         int randomSpawn = 0;
-        randomSpawn = Random.Range(0, SpawnPoint.Length);
-        if (CountLevel_1 < Pools[0].Count)
+        randomSpawn = UnityEngine.Random.Range(0, SpawnPoint.Length);
+        if (CountMonster[monsterKind] < DeActive[monsterKind].Count)
         {
-            spawn = Pools[0][CountLevel_1];
-            spawn.transform.position = SpawnPosition(SpawnPoint[randomSpawn], spawn).position;
+            spawn = DeActive[0][CountMonster[monsterKind]];
+            spawn.transform.position = SpawnPoint[randomSpawn].transform.position;
             spawn.SetActive(true);
-            CountLevel_1++;
+            CountMonster[monsterKind]++;
         }
-
-        return spawn;
-    }
-
-    /// <summary>
-    /// 레벨 2 몬스터 활성화
-    /// </summary>
-    /// <returns></returns>
-    private GameObject SpawnLevel_2()
-    {
-        GameObject spawn = null;
-        int randomSpawn = 0;
-        randomSpawn = Random.Range(0, SpawnPoint.Length);
-        if (CountLevel_2 < Pools[1].Count)
-        {
-            spawn = Pools[1][CountLevel_2];
-            spawn.transform.position = SpawnPosition(SpawnPoint[randomSpawn], spawn).position;
-
-            spawn.SetActive(true);
-            CountLevel_2++;
-        }
-        return spawn;
-    }
-
-    /// <summary>
-    /// 레벨 3 몬스터 활성화
-    /// </summary>
-    /// <returns></returns>
-    private GameObject SpawnLevel_3()
-    {
-        GameObject spawn = null;
-        int randomSpawn = 0;
-        randomSpawn = Random.Range(0, SpawnPoint.Length);
-        if (CountLevel_3 < Pools[2].Count)
-        {
-            spawn = Pools[2][CountLevel_3];
-            spawn.transform.position = SpawnPosition(SpawnPoint[randomSpawn], spawn).position;
-
-            spawn.SetActive(true);
-            CountLevel_3++;
-        }
-        return spawn;
-    }
-
-    /// <summary>
-    /// 레벨 4 몬스터 활성화
-    /// </summary>
-    /// <returns></returns>
-    private GameObject SpawnLevel_4()
-    {
-        GameObject spawn = null;
-        int randomSpawn = 0;
-        randomSpawn = Random.Range(0, SpawnPoint.Length);
-        if (CountLevel_4 < Pools[3].Count)
-        {
-            spawn = Pools[3][CountLevel_4];
-            spawn.transform.position = SpawnPosition(SpawnPoint[randomSpawn], spawn).position;
-
-            spawn.SetActive(true);
-            CountLevel_4++;
-        }
-        return spawn;
-    }
-
-    /// <summary>
-    /// 몬스터 스폰할 위치 반환해줌
-    /// </summary>
-    /// <param name="spawnObject">스폰 포인트 게임 오브젝트</param>
-    /// <returns></returns>
-    private Transform SpawnPosition(GameObject spawnObject, GameObject _spawn)
-    {
-        Transform spawn = _spawn.transform;
-        RaycastHit hit;
-
-        Debug.DrawRay(spawnObject.transform.position, Vector3.down * 500f, Color.magenta);
-        if (Physics.Raycast(spawnObject.transform.position, Vector3.down, out hit))
-        {
-            Debug.Log("DOWN 스폰포인트 레이 감지");
-            spawn.position = hit.point;
-        }
-
         return spawn;
     }
 }
